54850141c72be3b908e16fa35fa92a74f158523d
dadamu <p22626262@gmail.com>
2026-03-13 16:30:57 +0800

    chore: remove unused index.ts

--- src/index.ts
+++
@@ -1,4 +0,0 @@
-export { ExampleButton } from "./components/ExampleButton";
-export type { ExampleButtonProps } from "./components/ExampleButton";
-export { ExploreAppCard } from "./components/ExplorerAppCard";
-export type { ExploreAppCardProps } from "./components/ExplorerAppCard";
