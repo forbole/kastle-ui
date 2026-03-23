439ab5e7c1610614f6f87a13c5830e9003c17ddf
GitHub Action <action@github.com>
2026-03-23 09:43:31 +0000

    Clean up preview for PR #15

--- preview/pr-15/assets/ExploreUrlBarMenu.stories-BtmoBvjI.js
+++
@@ -1,5 +0,0 @@
-import{E as r}from"./ExploreUrlBarMenu-BfzDahuE.js";import"./theme-BTxkoYAB.js";import"./iframe-B1TJtXcK.js";import"./preload-helper-Zf8nSx-t.js";import"./createLucideIcon-CPfic1fo.js";const c={title:"Components/ExploreUrlBarMenu",component:r,argTypes:{onClose:{action:"closed"},onSharePress:{action:"share pressed"},onDisconnectPress:{action:"disconnect pressed"}}},e={args:{visible:!0}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
-  args: {
-    visible: true
-  }
-}`,...e.parameters?.docs?.source}}};const p=["Default"];export{e as Default,p as __namedExportsOrder,c as default};
