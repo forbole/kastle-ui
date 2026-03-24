ac2f1d5a2a1fd06e526a867fbb551a1442596513
GitHub Action <action@github.com>
2026-03-24 10:43:20 +0000

    Clean up preview for PR #29

--- preview/pr-29/assets/ExploreUrlBarMenu.stories-qNMSJ5oY.js
+++
@@ -1,5 +0,0 @@
-import{E as r}from"./ExploreUrlBarMenu-BFnxgUmv.js";import"./theme-DubikxOw.js";import"./iframe-WLDDlgDl.js";import"./preload-helper-Zf8nSx-t.js";import"./createLucideIcon-Bmde3Nq4.js";const p={title:"Explore/Components/ExploreUrlBarMenu",component:r,argTypes:{onClose:{action:"closed"},onSharePress:{action:"share pressed"},onDisconnectPress:{action:"disconnect pressed"}}},e={args:{visible:!0}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
-  args: {
-    visible: true
-  }
-}`,...e.parameters?.docs?.source}}};const c=["Default"];export{e as Default,c as __namedExportsOrder,p as default};
