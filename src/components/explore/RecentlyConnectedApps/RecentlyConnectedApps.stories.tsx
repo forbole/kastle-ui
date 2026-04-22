26d6a7e4e1c45904f334ebac380df310ac782f6d
Paul Chen <p22626262@gmail.com>
2026-04-22 15:55:40 +0800

    feat: replace ScrollView with FlatList and add peek scroll effect in RecentlyConnectedApps (#41)

--- src/components/explore/RecentlyConnectedApps/RecentlyConnectedApps.stories.tsx
+++ src/components/explore/RecentlyConnectedApps/RecentlyConnectedApps.stories.tsx
@@ -1,4 +1,5 @@
 import type { Meta, StoryObj } from "@storybook/react";
+import { View } from "react-native";
 import { RecentlyConnectedApps } from "./RecentlyConnectedApps";
 import iconImage from "../../../../assets/icon.png";
 
@@ -8,8 +9,14 @@ const meta: Meta<typeof RecentlyConnectedApps> = {
   argTypes: {
     onAppPress: { action: "app pressed" },
     onRemoveApp: { action: "remove app pressed" },
-    onManagePress: { action: "manage pressed" },
   },
+  decorators: [
+    (Story) => (
+      <View style={{ marginHorizontal: 20 }}>
+        <Story />
+      </View>
+    ),
+  ],
 };
 
 export default meta;
@@ -18,31 +25,18 @@ type Story = StoryObj<typeof meta>;
 export const Default: Story = {
   args: {
     apps: [
-      {
-        id: "0",
-        appName: "Moonbound",
-        appIcon: iconImage,
-      },
-      {
-        id: "1",
-        appName: "Kaspa Finance",
-        appIcon: iconImage,
-      },
-      {
-        id: "2",
-        appName: "Zealous Swap",
-        appIcon: iconImage,
-      },
-      {
-        id: "3",
-        appName: "KaspaCom",
-        appIcon: iconImage,
-      },
-      {
-        id: "4",
-        appName: "Another App",
-        appIcon: iconImage,
-      },
+      { id: "0", appName: "Moonbound", appIcon: iconImage },
+      { id: "1", appName: "Kaspa Finance", appIcon: iconImage },
+      { id: "2", appName: "Zealous Swap", appIcon: iconImage },
+      { id: "3", appName: "KaspaCom", appIcon: iconImage },
+      { id: "4", appName: "Another App", appIcon: iconImage },
+      { id: "5", appName: "DeFi Hub", appIcon: iconImage },
+      { id: "6", appName: "KasWallet", appIcon: iconImage },
+      { id: "7", appName: "SpeedSwap", appIcon: iconImage },
+      { id: "8", appName: "NanoTrade", appIcon: iconImage },
+      { id: "9", appName: "Kaspa DEX", appIcon: iconImage },
+      { id: "10", appName: "BlockBridge", appIcon: iconImage },
+      { id: "11", appName: "ChainVault", appIcon: iconImage },
     ],
   },
 };
@@ -87,3 +81,33 @@ export const WithoutIcons: Story = {
     ],
   },
 };
+
+export const SingleApp: Story = {
+  args: {
+    apps: [
+      {
+        id: "0",
+        appName: "Moonbound",
+        appIcon: iconImage,
+      },
+    ],
+  },
+};
+
+export const TwoApps: Story = {
+  args: {
+    apps: [
+      {
+        id: "0",
+        appName: "Moonbound",
+        appIcon: iconImage,
+      },
+      {
+        id: "1",
+        appName: "Kaspa Finance",
+        appIcon: iconImage,
+      },
+    ],
+  },
+};
+
