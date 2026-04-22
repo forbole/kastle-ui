ee92a33b4937c14fc2afb3c22959d2dcac16ab7d
dadamu <dadamu@users.noreply.github.com>
2026-04-22 06:53:18 +0000

    deploy: 2990ae39099ad681e1e63cf530f8bbbcb1e40fa0

---
+++ assets/ExploreUrlBarMenu.stories-Cef0zGR4.js
@@ -0,0 +1,5 @@
+import{E as r}from"./ExploreUrlBarMenu-DIln21_S.js";import"./theme-Dn_jto50.js";import"./iframe-DGkjb2d9.js";import"./preload-helper-Zf8nSx-t.js";import"./createLucideIcon-8wQXE3cN.js";const p={title:"Explore/Components/ExploreUrlBarMenu",component:r,argTypes:{onClose:{action:"closed"},onSharePress:{action:"share pressed"},onDisconnectPress:{action:"disconnect pressed"}}},e={args:{visible:!0}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  args: {
+    visible: true
+  }
+}`,...e.parameters?.docs?.source}}};const c=["Default"];export{e as Default,c as __namedExportsOrder,p as default};
