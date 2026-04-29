ae8f1d7502f315c7d4b51d83d73ac327b92b63ce
GitHub Action <action@github.com>
2026-04-29 09:07:04 +0000

    Clean up preview for PR #45

--- preview/pr-45/assets/ExploreUrlBarMenu.stories-DONkI-qp.js
+++
@@ -1,5 +0,0 @@
-import{E as r}from"./ExploreUrlBarMenu-Cqdm_CbW.js";import"./theme-Bi7plu6O.js";import"./iframe-CDyyuMcd.js";import"./preload-helper-Zf8nSx-t.js";import"./AppText-9106QR7_.js";import"./unplug-fM9FpglJ.js";import"./createLucideIcon-BV3i0aw5.js";import"./registry-BNXumi8c.js";const m={title:"Explore/Components/ExploreUrlBarMenu",component:r,argTypes:{onClose:{action:"closed"},onSharePress:{action:"share pressed"},onDisconnectPress:{action:"disconnect pressed"}}},e={args:{visible:!0}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
-  args: {
-    visible: true
-  }
-}`,...e.parameters?.docs?.source}}};const l=["Default"];export{e as Default,l as __namedExportsOrder,m as default};
--- preview/pr-45/assets/chevron-right-Bi7J9UtI.js
+++
@@ -1,1 +0,0 @@
-import{c as t}from"./createLucideIcon-BV3i0aw5.js";const h=t("ChevronRight",[["path",{d:"m9 18 6-6-6-6",key:"mthhwq"}]]);export{h as C};
