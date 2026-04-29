0be2d896757eea4868d92b8c3ce5dd67b09cb3bd
dadamu <dadamu@users.noreply.github.com>
2026-04-29 09:07:33 +0000

    deploy: ecc69c9c414c29c0c53b4e2f3bf49a58e8c015b1

---
+++ assets/ExploreAppCard.stories-Crxz6xIB.js
@@ -0,0 +1,37 @@
+import{E as i}from"./ExploreAppCard-CfOHX94X.js";import"./theme-Bi7plu6O.js";import"./iframe-CDyyuMcd.js";import"./preload-helper-Zf8nSx-t.js";import"./AppText-9106QR7_.js";import"./check-DVBkiO3B.js";import"./createLucideIcon-BV3i0aw5.js";import"./registry-BNXumi8c.js";import"./Image-B8Ze-LUj.js";import"./NativeEventEmitter-CVDQfLr9.js";const y={title:"Explore/Components/ExploreAppCard",component:i,argTypes:{isVerified:{control:{type:"boolean"}},onPress:{action:"pressed"}},args:{appName:"Zealous Swap",appCategory:"zealousswap.com",isVerified:!0,onPress:()=>console.log("App card pressed!")}},e={args:{appName:"Zealous Swap",appCategory:"zealousswap.com",isVerified:!0}},a={args:{appName:"DEX A",appCategory:"dex.com",isVerified:!1}},r={args:{appName:"KAT Bridge",appCategory:"Bridge",isVerified:!0}},p={args:{appName:"Kaspa Finance",appCategory:"DeFi",isVerified:!0}},o={args:{appName:"Moonbound",appCategory:"DeFi",isVerified:!0}},s={args:{appName:"Super Long Application Name That Should Be Truncated",appCategory:"Very Long Category Name",isVerified:!1}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  args: {
+    appName: "Zealous Swap",
+    appCategory: "zealousswap.com",
+    isVerified: true
+  }
+}`,...e.parameters?.docs?.source}}};a.parameters={...a.parameters,docs:{...a.parameters?.docs,source:{originalSource:`{
+  args: {
+    appName: "DEX A",
+    appCategory: "dex.com",
+    isVerified: false
+  }
+}`,...a.parameters?.docs?.source}}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  args: {
+    appName: "KAT Bridge",
+    appCategory: "Bridge",
+    isVerified: true
+  }
+}`,...r.parameters?.docs?.source}}};p.parameters={...p.parameters,docs:{...p.parameters?.docs,source:{originalSource:`{
+  args: {
+    appName: "Kaspa Finance",
+    appCategory: "DeFi",
+    isVerified: true
+  }
+}`,...p.parameters?.docs?.source}}};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  args: {
+    appName: "Moonbound",
+    appCategory: "DeFi",
+    isVerified: true
+  }
+}`,...o.parameters?.docs?.source}}};s.parameters={...s.parameters,docs:{...s.parameters?.docs,source:{originalSource:`{
+  args: {
+    appName: "Super Long Application Name That Should Be Truncated",
+    appCategory: "Very Long Category Name",
+    isVerified: false
+  }
+}`,...s.parameters?.docs?.source}}};const C=["VerifiedApp","UnverifiedApp","BridgeApp","FinanceApp","MoonboundApp","LongName"];export{r as BridgeApp,p as FinanceApp,s as LongName,o as MoonboundApp,a as UnverifiedApp,e as VerifiedApp,C as __namedExportsOrder,y as default};
