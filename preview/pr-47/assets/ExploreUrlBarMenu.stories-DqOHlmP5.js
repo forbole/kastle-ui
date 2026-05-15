5ebfa6579692764a66686a04c0c3f55c115c7f22
GitHub Action <action@github.com>
2026-05-15 06:40:05 +0000

    Clean up preview for PR #47

--- preview/pr-47/assets/ExploreUrlBarMenu.stories-DqOHlmP5.js
+++
@@ -1,5 +0,0 @@
-import{E as e}from"./ExploreUrlBarMenu-B5l0OrG8.js";import"./theme-Br3UXtQO.js";import"./iframe-JF4SOhmV.js";import"./preload-helper-Zf8nSx-t.js";import"./AppText-CDytHUIv.js";import"./index-CQPoNSoe.js";import"./index-CAMOA1qm.js";import"./extends-CF3RwP-h.js";import"./unplug-Bk07oUbv.js";import"./createLucideIcon-BQiQhgXK.js";import"./registry-BNXumi8c.js";const u={title:"Explore/Components/ExploreUrlBarMenu",component:e,argTypes:{onClose:{action:"closed"},onSharePress:{action:"share pressed"},onDisconnectPress:{action:"disconnect pressed"}}},r={args:{visible:!0}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
-  args: {
-    visible: true
-  }
-}`,...r.parameters?.docs?.source}}};const x=["Default"];export{r as Default,x as __namedExportsOrder,u as default};
