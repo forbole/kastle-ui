2b4c781fcdae2ef7b4c4bb4df5f336bdd5f9d51f
dadamu <dadamu@users.noreply.github.com>
2026-05-29 06:49:08 +0000

    deploy: b84d4b08a946c18af7a96de03831f10600cd65b0

---
+++ assets/ExploreUrlBarMenu.stories-B4OFhMFQ.js
@@ -0,0 +1,5 @@
+import{E as e}from"./ExploreUrlBarMenu-C02wokPp.js";import"./theme-DsIVqOpk.js";import"./iframe-Be-tza-6.js";import"./preload-helper-Zf8nSx-t.js";import"./index-CSVpXL3J.js";import"./extends-CF3RwP-h.js";import"./index-D3gmiGVC.js";import"./index-CQ0gql0M.js";import"./unplug-DMjWbbO1.js";import"./createLucideIcon-Bg0PyGW2.js";import"./registry-BNXumi8c.js";const u={title:"Explore/Components/ExploreUrlBarMenu",component:e,argTypes:{onClose:{action:"closed"},onSharePress:{action:"share pressed"},onDisconnectPress:{action:"disconnect pressed"}}},r={args:{visible:!0}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  args: {
+    visible: true
+  }
+}`,...r.parameters?.docs?.source}}};const x=["Default"];export{r as Default,x as __namedExportsOrder,u as default};
---
+++ assets/WalletOptionButton.stories-L8sdZpje.js
@@ -0,0 +1,15 @@
+import{j as t,V as l,s as n,a as o,b as p}from"./theme-DsIVqOpk.js";import{W as m}from"./WalletOptionButton-kF5pt3s1.js";import"./iframe-Be-tza-6.js";import"./preload-helper-Zf8nSx-t.js";import"./index-Bdrro2xU.js";import"./extends-CF3RwP-h.js";import"./index-D3gmiGVC.js";import"./index-CQ0gql0M.js";import"./arrow-right-Bz5vECbR.js";import"./createLucideIcon-Bg0PyGW2.js";import"./registry-BNXumi8c.js";const I={title:"Add-Wallets/Components/WalletOptionButton",component:m,parameters:{backgrounds:{default:"kastle"},viewport:{defaultViewport:"iphone14"}},decorators:[s=>t.jsx(l,{style:c.decorator,children:t.jsx(s,{})})],args:{onPress:()=>{}}},e={args:{label:"Create Wallet"}},r={args:{label:"Import wallet"}},a={args:{label:"Import Hardware wallet",badge:"Soon",disabled:!0}},c=n.create({decorator:{flex:1,justifyContent:"center",backgroundColor:p.bg0,padding:o.s5,gap:o.s4}});e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  args: {
+    label: "Create Wallet"
+  }
+}`,...e.parameters?.docs?.source}}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  args: {
+    label: "Import wallet"
+  }
+}`,...r.parameters?.docs?.source}}};a.parameters={...a.parameters,docs:{...a.parameters?.docs,source:{originalSource:`{
+  args: {
+    label: "Import Hardware wallet",
+    badge: "Soon",
+    disabled: true
+  }
+}`,...a.parameters?.docs?.source}}};const j=["CreateWallet","ImportWallet","HardwareSoon"];export{e as CreateWallet,a as HardwareSoon,r as ImportWallet,j as __namedExportsOrder,I as default};
