69aa58dffcebdfb4c672776bf85d85b467e7bd09
GitHub Action <action@github.com>
2026-07-15 09:33:58 +0000

    Clean up preview for PR #52

--- preview/pr-52/assets/ActivitySkeletonRow.stories-BqayDjoe.js
+++
@@ -1,8 +0,0 @@
-import{j as e,V as i,s as a,b as n}from"./theme-DglJhicK.js";import{A as r}from"./ActivitySkeletonRow-BzeRlcAZ.js";import"./iframe-CU-Zjm2b.js";import"./preload-helper-Zf8nSx-t.js";import"./SkeletonBlock-B5GYe-2e.js";import"./Animated-BFjZoZkP.js";import"./extends-CF3RwP-h.js";import"./index-CV6nhDdt.js";import"./index-CXWo9_IW.js";import"./index-14LLgglL.js";import"./index-KoLHHI-L.js";import"./NativeEventEmitter-jc1HuR-1.js";import"./index-C4tRQv6Y.js";import"./index-Bz88AlIB.js";const v={title:"Swap-bridge-activity/Components/ActivitySkeletonRow",component:r,parameters:{backgrounds:{default:"kastle"},viewport:{defaultViewport:"iphone14"}},decorators:[s=>e.jsx(i,{style:c.decorator,children:e.jsx(s,{})})]},o={},t={render:()=>e.jsxs(e.Fragment,{children:[e.jsx(r,{}),e.jsx(r,{}),e.jsx(r,{}),e.jsx(r,{})]})},c=a.create({decorator:{flex:1,backgroundColor:n.bg0,padding:20}});o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:"{}",...o.parameters?.docs?.source},description:{story:"Single skeleton row",...o.parameters?.docs?.description}}};t.parameters={...t.parameters,docs:{...t.parameters?.docs,source:{originalSource:`{
-  render: () => <>
-      <ActivitySkeletonRow />
-      <ActivitySkeletonRow />
-      <ActivitySkeletonRow />
-      <ActivitySkeletonRow />
-    </>
-}`,...t.parameters?.docs?.source},description:{story:"List of 4 skeleton rows",...t.parameters?.docs?.description}}};const A=["SingleRow","ListOfFour"];export{t as ListOfFour,o as SingleRow,A as __namedExportsOrder,v as default};
--- preview/pr-52/assets/ExploreUrlBarMenu.stories-Dm8wSsVW.js
+++
@@ -1,5 +0,0 @@
-import{E as e}from"./ExploreUrlBarMenu-D4HZOq1h.js";import"./theme-DglJhicK.js";import"./iframe-CU-Zjm2b.js";import"./preload-helper-Zf8nSx-t.js";import"./index-6GpvJ_gk.js";import"./extends-CF3RwP-h.js";import"./index-Bz88AlIB.js";import"./share-2-B5bcGoBY.js";import"./createLucideIcon-BD6Rl3dZ.js";import"./registry-BNXumi8c.js";import"./index-CXWo9_IW.js";import"./unplug-ttCPxtV1.js";const x={title:"Explore/Components/ExploreUrlBarMenu",component:e,argTypes:{onClose:{action:"closed"},onSharePress:{action:"share pressed"},onDisconnectPress:{action:"disconnect pressed"}}},r={args:{visible:!0}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
-  args: {
-    visible: true
-  }
-}`,...r.parameters?.docs?.source}}};const E=["Default"];export{r as Default,E as __namedExportsOrder,x as default};
