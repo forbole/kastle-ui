8a38a5135a619db55da218ee8e5106bae533911e
dadamu <dadamu@users.noreply.github.com>
2026-07-15 08:05:13 +0000

    deploy: 026c5f830bba03e4531cf1b258fc3622fccf47bb

---
+++ assets/ActivitySkeletonRow.stories-C5QPlgx0.js
@@ -0,0 +1,8 @@
+import{j as e,V as i,s as a,b as n}from"./theme-C0agyO19.js";import{A as r}from"./ActivitySkeletonRow-D1ypwrrB.js";import"./iframe-CNZKlBxT.js";import"./preload-helper-Zf8nSx-t.js";import"./SkeletonBlock-C_y2bXUa.js";import"./Animated-DAtxQpih.js";import"./extends-CF3RwP-h.js";import"./index-DQNekZOj.js";import"./index-DWck0XxJ.js";import"./index-DFM0n4mV.js";import"./NativeEventEmitter-BnP8nzRW.js";import"./index-E3fOUPlz.js";import"./index-Cnw2gWmN.js";const R={title:"Swap-bridge-activity/Components/ActivitySkeletonRow",component:r,parameters:{backgrounds:{default:"kastle"},viewport:{defaultViewport:"iphone14"}},decorators:[s=>e.jsx(i,{style:c.decorator,children:e.jsx(s,{})})]},o={},t={render:()=>e.jsxs(e.Fragment,{children:[e.jsx(r,{}),e.jsx(r,{}),e.jsx(r,{}),e.jsx(r,{})]})},c=a.create({decorator:{flex:1,backgroundColor:n.bg0,padding:20}});o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:"{}",...o.parameters?.docs?.source},description:{story:"Single skeleton row",...o.parameters?.docs?.description}}};t.parameters={...t.parameters,docs:{...t.parameters?.docs,source:{originalSource:`{
+  render: () => <>
+      <ActivitySkeletonRow />
+      <ActivitySkeletonRow />
+      <ActivitySkeletonRow />
+      <ActivitySkeletonRow />
+    </>
+}`,...t.parameters?.docs?.source},description:{story:"List of 4 skeleton rows",...t.parameters?.docs?.description}}};const v=["SingleRow","ListOfFour"];export{t as ListOfFour,o as SingleRow,v as __namedExportsOrder,R as default};
