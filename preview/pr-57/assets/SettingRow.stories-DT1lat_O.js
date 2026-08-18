193bb1b3f6046564bc5f7b35fa8fa307886a68b2
nicoletting <nicoletting@users.noreply.github.com>
2026-08-18 09:42:31 +0000

    deploy: cad92c3ff1ac44854529ca89f7725254cddef26a

---
+++ preview/pr-57/assets/SettingRow.stories-DT1lat_O.js
@@ -0,0 +1,23 @@
+import{j as e,V as n,c as l,d as u}from"./theme-BOKYgWy2.js";import{S as t}from"./SettingRow-KIiPmwUQ.js";import"./iframe-Ogo3UuBR.js";import"./preload-helper-Zf8nSx-t.js";import"./index-CwKHlYOk.js";import"./extends-CF3RwP-h.js";import"./index-JBCSNUA2.js";import"./chevron-right-CeIOIsP_.js";import"./createLucideIcon-B9vLRcAl.js";import"./registry-BNXumi8c.js";import"./index-ErkzclZf.js";const S={title:"Components/SettingRow",component:t,decorators:[c=>e.jsx(n,{style:{width:"100%",padding:20,backgroundColor:l.backgroundScreen},children:e.jsx(n,{style:{backgroundColor:l.backgroundSurface,borderRadius:u["2xl"],overflow:"hidden"},children:e.jsx(c,{})})})]},r={args:{label:"Custom RPC",value:"Default",onPress:()=>{}}},o={args:{label:"Change PIN",onPress:()=>{}}},s={args:{label:"Custom RPC",value:"A very long node's name that truncates",onPress:()=>{}}},a={render:()=>e.jsxs(e.Fragment,{children:[e.jsx(t,{label:"Network",value:"Mainnet",onPress:()=>{}}),e.jsx(t,{label:"Custom RPC",value:"Default",showTopDivider:!0,onPress:()=>{}})]})};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  args: {
+    label: "Custom RPC",
+    value: "Default",
+    onPress: () => {}
+  }
+}`,...r.parameters?.docs?.source}}};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  args: {
+    label: "Change PIN",
+    onPress: () => {}
+  }
+}`,...o.parameters?.docs?.source}}};s.parameters={...s.parameters,docs:{...s.parameters?.docs,source:{originalSource:`{
+  args: {
+    label: "Custom RPC",
+    value: "A very long node's name that truncates",
+    onPress: () => {}
+  }
+}`,...s.parameters?.docs?.source}}};a.parameters={...a.parameters,docs:{...a.parameters?.docs,source:{originalSource:`{
+  render: () => <>
+      <SettingRow label="Network" value="Mainnet" onPress={() => {}} />
+      <SettingRow label="Custom RPC" value="Default" showTopDivider onPress={() => {}} />
+    </>
+}`,...a.parameters?.docs?.source}}};const f=["Default","ChevronOnly","LongValue","Stacked"];export{o as ChevronOnly,r as Default,s as LongValue,a as Stacked,f as __namedExportsOrder,S as default};
