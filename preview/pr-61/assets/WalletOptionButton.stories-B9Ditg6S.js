ed1aee4b2679660e27f0ac7da6a6dceea581f923
nicoletting <nicoletting@users.noreply.github.com>
2026-08-26 05:20:05 +0000

    deploy: 26c508fa06a89fd6828fe5078f8fd39a62298743

---
+++ preview/pr-61/assets/WalletOptionButton.stories-B9Ditg6S.js
@@ -0,0 +1,20 @@
+import{j as o,V as n,s as p,a as s,b as c}from"./theme-CI3XLjdX.js";import{W as d}from"./WalletOptionButton-CarKO1ih.js";import"./iframe-BRTfYPGp.js";import"./preload-helper-Zf8nSx-t.js";import"./index-CJSFGT_l.js";import"./extends-CF3RwP-h.js";import"./index-DjhBL4d3.js";import"./index-QfDaSQVD.js";import"./arrow-right-Ba03g3XY.js";import"./createLucideIcon-B1McIo9u.js";import"./registry-BNXumi8c.js";import"./index-ClyrT4qx.js";const k={title:"Add-Wallets/Components/WalletOptionButton",component:d,parameters:{backgrounds:{default:"kastle"},viewport:{defaultViewport:"iphone14"}},decorators:[l=>o.jsx(n,{style:m.decorator,children:o.jsx(l,{})})],args:{onPress:()=>{}}},e={args:{label:"Create Wallet"}},r={args:{label:"Import wallet"}},a={args:{label:"Import Hardware wallet",badge:"Soon",disabled:!0}},t={args:{label:"Create Wallet",isLoading:!0}},m=p.create({decorator:{flex:1,justifyContent:"center",backgroundColor:c.bg0,padding:s.s5,gap:s.s4}});e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  args: {
+    label: "Create Wallet"
+  }
+}`,...e.parameters?.docs?.source}}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  args: {
+    label: "Import wallet"
+  }
+}`,...r.parameters?.docs?.source}}};a.parameters={...a.parameters,docs:{...a.parameters?.docs,source:{originalSource:`{
+  args: {
+    label: "Import Hardware wallet",
+    badge: "Soon",
+    disabled: true
+  }
+}`,...a.parameters?.docs?.source}}};t.parameters={...t.parameters,docs:{...t.parameters?.docs,source:{originalSource:`{
+  args: {
+    label: "Create Wallet",
+    isLoading: true
+  }
+}`,...t.parameters?.docs?.source}}};const y=["CreateWallet","ImportWallet","HardwareSoon","Loading"];export{e as CreateWallet,a as HardwareSoon,r as ImportWallet,t as Loading,y as __namedExportsOrder,k as default};
---
+++ preview/pr-61/assets/search-Cmi-h8Cm.js
@@ -0,0 +1,1 @@
+import{c}from"./createLucideIcon-B1McIo9u.js";const r=c("Search",[["path",{d:"m21 21-4.34-4.34",key:"14j7rj"}],["circle",{cx:"11",cy:"11",r:"8",key:"4ej97u"}]]);export{r as S};
