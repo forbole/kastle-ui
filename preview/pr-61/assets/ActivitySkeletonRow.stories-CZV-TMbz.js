c6aaf1433b91aa10f8eada6d037e9e40bd0d5533
GitHub Action <action@github.com>
2026-08-26 11:45:36 +0000

    Clean up preview for PR #61

--- preview/pr-61/assets/ActivitySkeletonRow.stories-CZV-TMbz.js
+++
@@ -1,8 +0,0 @@
-import{j as e,V as i,s as a,b as n}from"./theme-CI3XLjdX.js";import{A as r}from"./ActivitySkeletonRow-BNSecEXg.js";import"./iframe-BRTfYPGp.js";import"./preload-helper-Zf8nSx-t.js";import"./SkeletonBlock-DLDfDflM.js";import"./Animated-C3vtFFZ4.js";import"./extends-CF3RwP-h.js";import"./index-DFTc8yWS.js";import"./index-ClyrT4qx.js";import"./index-C1Wf_w99.js";import"./index-Ca2rKSTy.js";import"./NativeEventEmitter-B8zbeVp5.js";import"./index-XozokZnn.js";import"./index-DjhBL4d3.js";const v={title:"Swap-bridge-activity/Components/ActivitySkeletonRow",component:r,parameters:{backgrounds:{default:"kastle"},viewport:{defaultViewport:"iphone14"}},decorators:[s=>e.jsx(i,{style:c.decorator,children:e.jsx(s,{})})]},o={},t={render:()=>e.jsxs(e.Fragment,{children:[e.jsx(r,{}),e.jsx(r,{}),e.jsx(r,{}),e.jsx(r,{})]})},c=a.create({decorator:{flex:1,backgroundColor:n.bg0,padding:20}});o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:"{}",...o.parameters?.docs?.source},description:{story:"Single skeleton row",...o.parameters?.docs?.description}}};t.parameters={...t.parameters,docs:{...t.parameters?.docs,source:{originalSource:`{
-  render: () => <>
-      <ActivitySkeletonRow />
-      <ActivitySkeletonRow />
-      <ActivitySkeletonRow />
-      <ActivitySkeletonRow />
-    </>
-}`,...t.parameters?.docs?.source},description:{story:"List of 4 skeleton rows",...t.parameters?.docs?.description}}};const A=["SingleRow","ListOfFour"];export{t as ListOfFour,o as SingleRow,A as __namedExportsOrder,v as default};
