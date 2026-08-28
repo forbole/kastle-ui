28cc6a474be41f659162dc5670da7cf5095cae29
leobragaz <leobragaz@users.noreply.github.com>
2026-08-28 14:11:48 +0000

    deploy: d95abbb21b2863bf96ade4d5f8731d00cc6f1fe1

---
+++ preview/pr-63/assets/ExploreUrlBarMenu.stories-CA7CQPf7.js
@@ -0,0 +1,5 @@
+import{E as e}from"./ExploreUrlBarMenu-BOH25X9c.js";import"./theme-8QOGWT04.js";import"./iframe-zWxnyr7p.js";import"./preload-helper-Zf8nSx-t.js";import"./index-DR7f6Xxm.js";import"./extends-CF3RwP-h.js";import"./index-CJru1tUL.js";import"./share-2-C3cDgYCP.js";import"./createLucideIcon-Ddhol5HT.js";import"./registry-BNXumi8c.js";import"./index-BDF7tMil.js";import"./unplug-BtqZ8dKm.js";const x={title:"Explore/Components/ExploreUrlBarMenu",component:e,argTypes:{onClose:{action:"closed"},onSharePress:{action:"share pressed"},onDisconnectPress:{action:"disconnect pressed"}}},r={args:{visible:!0}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  args: {
+    visible: true
+  }
+}`,...r.parameters?.docs?.source}}};const E=["Default"];export{r as Default,E as __namedExportsOrder,x as default};
