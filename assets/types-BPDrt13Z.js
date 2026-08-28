c3ff3d8fb6c05d6e46986ad3aaeb99fd6997a594
leobragaz <leobragaz@users.noreply.github.com>
2026-08-28 14:11:53 +0000

    deploy: 10b36f1a910ba2ba89f6b87ec05195ef08cb2cfc

---
+++ assets/types-BPDrt13Z.js
@@ -0,0 +1,1 @@
+import{Y as a,p as r}from"./theme-8QOGWT04.js";const s={kns:{label:"Kaspa",suffix:".kas",color:r.p500,background:r.p0},igra:{label:"iGRA",suffix:".igra",color:a.t500,background:a.t0}};export{s as F};
