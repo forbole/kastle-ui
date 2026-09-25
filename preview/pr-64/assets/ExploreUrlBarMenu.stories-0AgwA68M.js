08f79eb3e7477fda244386fba0b6bed9c5302a2f
GitHub Action <action@github.com>
2026-09-25 23:57:51 +0000

    Clean up preview for PR #64

--- preview/pr-64/assets/ExploreUrlBarMenu.stories-0AgwA68M.js
+++
@@ -1,5 +0,0 @@
-import{E as e}from"./ExploreUrlBarMenu-BGoX6ZPB.js";import"./theme-CMj5SAcx.js";import"./iframe-Dlitb8YL.js";import"./preload-helper-Zf8nSx-t.js";import"./index-DM9C-eE_.js";import"./extends-CF3RwP-h.js";import"./share-2-D-gwZ_-b.js";import"./createLucideIcon-CsQHbI7R.js";import"./registry-BNXumi8c.js";import"./index-Dl0GLfCf.js";import"./unplug-CI-P9p5t.js";const u={title:"Explore/Components/ExploreUrlBarMenu",component:e,argTypes:{onClose:{action:"closed"},onSharePress:{action:"share pressed"},onDisconnectPress:{action:"disconnect pressed"}}},r={args:{visible:!0}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
-  args: {
-    visible: true
-  }
-}`,...r.parameters?.docs?.source}}};const x=["Default"];export{r as Default,x as __namedExportsOrder,u as default};
